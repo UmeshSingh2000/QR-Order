9283b3f6fc2bddc08a456a52bb12cf8c1e56ca18
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-16 16:31:16 +0530

    Add server endpoint and admin schema model

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,9 @@ app.use(express.json())
 
 
 
-
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
