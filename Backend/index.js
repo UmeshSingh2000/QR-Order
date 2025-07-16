824fbd757c90089f9aeaa66daf637b77a9ca317c
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-16 16:41:08 +0530

    Add database connection logic to the server initialization

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 require('dotenv').config();
+const connectDB = require('./Database/connection');
+connectDB();
 const port = process.env.PORT || 3000;
 app.use(express.json())
 
