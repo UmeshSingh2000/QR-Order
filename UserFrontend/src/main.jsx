ea3cced4a38f5aee227c8f21dc2bbe0080043199
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-17 00:58:55 +0530

    feat: add react-router-dom for dynamic routing and update App component to use table number from URL

--- UserFrontend/src/main.jsx
+++ UserFrontend/src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/:tableNumber' element={<App />} />
+        </Routes>
+    </BrowserRouter>
 )
