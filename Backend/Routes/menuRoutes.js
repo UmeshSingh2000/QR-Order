3ae34aa51cd505a6f202c7c8d9ebb3d391b4fb78
Umesh singh <umeshsinghmehta4@gmail.com>
2025-07-16 18:55:31 +0530

    fix: update deleteSection endpoint to use URL parameters for section name

--- Backend/Routes/menuRoutes.js
+++ Backend/Routes/menuRoutes.js
@@ -6,5 +6,5 @@ router.get('/getMenuItem',getMenuItem)
 router.post('/addMenuItem', addMenuItem);
 router.post('/addMultipleMenuItems',addMultipleMenuItems)
 router.delete('/deleteMenuItem/:sectionname/:itemname', deleteMenuItem);
-router.delete('/deleteSection', deleteSection);
+router.delete('/deleteSection/:sectionname', deleteSection);
 module.exports = router;
